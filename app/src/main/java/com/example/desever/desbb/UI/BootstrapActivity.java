99fd403e0ed380dc7e94bdc68db4d2709e44b39e
zhao <1446514143@qq.com>
2018-10-26 23:10:12 +0800

    1、增加底部菜单切换；2、增加侧边栏

--- app/src/main/java/com/example/desever/desbb/UI/BootstrapActivity.java
+++ app/src/main/java/com/example/desever/desbb/UI/BootstrapActivity.java
@@ -1,9 +1,16 @@
 package com.example.desever.desbb.UI;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
 import com.example.desever.desbb.R;
 import com.example.desever.desbb.component.DesTittleBarView;
+import com.example.desever.desbb.fragment.ChartFragment;
 import com.example.desever.desbb.fragment.HomeFragment;
+import com.example.desever.desbb.fragment.HotFragment;
 import com.example.desever.desbb.libs.DesActivity;
 
 
@@ -12,30 +19,42 @@ public class BootstrapActivity extends DesActivity {
     //首页导航
     DesTittleBarView homeNav;
 
+    //侧边栏
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bootstrap);
 
+        //页面初始化
+        initView();
+
+        //初始化顶部导航点击事件
+        initNavButtonClick();
+        //初始化底部导航点击
+        initBottomNavClick();
+        //初始化fragment页面
+        changeFragmentPage("home");
+    }
+
+
+    //页面初始化工作
+    private void initView(){
         //设置导航padding
         homeNav=findViewById(R.id.home_nav);
         setImmerseLayout(homeNav);
-
-        //添加布局
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.main_fragment_container,new HomeFragment())
-                .commit();
-        //初始化导航点击事件
-        initNavButtonClick();
-
+        //获取侧边栏
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
     }
+
 
     //导航点击事件
     private void initNavButtonClick(){
         homeNav.setLeftCustomCallBack(new DesTittleBarView.CustomCallBack() {
             @Override
             public void onclick(View v) {
-                toastLong("左边按钮");
+                showDrawerLayout();
             }
         });
         homeNav.setRightCustomCallBack(new DesTittleBarView.CustomCallBack() {
@@ -45,6 +64,72 @@ public class BootstrapActivity extends DesActivity {
             }
         });
     }
+
+
+    //监听底部导航点击事件
+    private void initBottomNavClick(){
+        RadioGroup bottomNav = (RadioGroup) findViewById(R.id.main_navi_radiogroup);
+        bottomNav.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.bottom_nav_home:
+                        changeFragmentPage("home");
+                        break;
+                    case R.id.bottom_nav_hot:
+                        changeFragmentPage("hot");
+                        break;
+                    case R.id.bottom_nav_our:
+                        changeFragmentPage("user");
+                        break;
+                }
+            }
+        });
+    }
+
+
+    //初始化Fragment页面
+    private void changeFragmentPage(String pageName){
+        //导航标题组件
+        TextView centerText=homeNav.findViewById(R.id.title_bar_title);
+        if(pageName=="home"){
+            //添加布局
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_fragment_container,new HomeFragment())
+                    .commit();
+
+            //修改标题文字
+            centerText.setText("首页");
+
+        }else if(pageName=="hot"){
+            //添加布局
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_fragment_container,new HotFragment())
+                    .commit();
+
+            //修改标题文字
+            centerText.setText("报告");
+        }else{
+            //添加布局
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_fragment_container,new ChartFragment())
+                    .commit();
+            //修改标题文字
+            centerText.setText("统计");
+        }
+
+    }
+
+    //关闭和打开侧边栏
+    private void showDrawerLayout() {
+        if (!drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            drawerLayout.openDrawer(Gravity.LEFT);
+        } else {
+            drawerLayout.closeDrawer(Gravity.LEFT);
+        }
+    }
+
+
 
 }
 
---
+++ app/src/main/java/com/example/desever/desbb/fragment/ChartFragment.java
@@ -0,0 +1,20 @@
+package com.example.desever.desbb.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.desever.desbb.R;
+
+public class ChartFragment extends Fragment {
+    //获取当前fragmen容器
+    View fragmenParent;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        fragmenParent = inflater.inflate(R.layout.chart_fragment, container, false);
+        return fragmenParent;
+    }
+}
